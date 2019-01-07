c8e976d26c0dae1a4be3b88aaf649ece02f36170
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-06 22:46:02 -0500

    layout, started js

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,4 +1,16 @@
 $(document).ready(function() {
+  // my giphyApi
+  var giphyApi = "BPXIt32b6aJqQBh3JzsEp3oaGeTjKYtW"
+
+  // setting up giphy queryURL variable with beginning of url
+  var queryURL = "https://api.giphy.com/v1/gifs/";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function(response) {
+    // console.log(response);
+  });
 
 
 
