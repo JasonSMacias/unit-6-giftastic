c7283bba1b0e5da9bf4d500d9cd4ac4f4c58b51c
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-07 22:26:47 -0500

    set up buttons to populate still images of fixed width gifs on screen

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -3,25 +3,43 @@ $(document).ready(function() {
   var giphyApi = "&api_key=BPXIt32b6aJqQBh3JzsEp3oaGeTjKYtW&limit=10"
 
   // setting up giphy queryURL variable with beginning of url
-  var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
+  var beginningURL = "https://api.giphy.com/v1/gifs/search?q=";
 
   // setting up array with strings for buttons
-  var topics = ["Darth Vader", "Princess Leia", "Luke Skywalker", "Obi-Wan Kenobi", "Han Solo", "R2-D2", "C-3PO", "Chewbacca", "Grand Moff Tarkin", "Emperor Palpatine", "Lando Calrissian", "Yoda", "Boba Fett", "Jabba the Hutt", "Admiral Ackbar", "Wicket (ewok)"];
+  var topics = ["Darth Vader", "Princess Leia", "Luke Skywalker", "Obi-Wan Kenobi", "Han Solo", "R2-D2", "C-3PO", "Chewbacca", "Moff Tarkin", "Emperor Palpatine", "Lando Calrissian", "Yoda", "Boba Fett", "Jabba the Hutt", "Admiral Ackbar", "Wicket (ewok)"];
 
   for (i=0; i < topics.length; i++){
-    $("#gifbuttons").append(`<button class="btn btn-outline-dark btn-sm" type="button" name="" value="">${topics[i]}</button>`);
+    $("#gifbuttons").append(`<button class="btn btn-outline-dark btn-sm gifbutton" type="button" data-character="${topics[i]}">${topics[i]}</button>`);
   };
 
+  // function for button clicks
+  $(".gifbutton").click(function(){
+    var character = $(this).attr("data-character");
+    queryURL = beginningURL+character+giphyApi;
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      console.log(response);
+      console.log(response.data);
 
+      for (i=0; i< response.data.length; i++){
+        console.log(response.data[i].images.fixed_width_still.url);
+        var characterImage = $("<img>").attr("class", "rounded").attr("class","m-2");
+        var rating = response.data[i].rating
+        console.log(rating);
+        var p = $("<p>").text("Rating: "+rating);
+        characterImage.attr("src", response.data[i].images.fixed_width_still.url);
+        $("#gifs").prepend(characterImage);
+      }
 
+    });
 
-
-
-  // $.ajax({
-  //   url: queryURL,
-  //   method: "GET"
-  // }).then(function(response) {
-  //   // console.log(response);
-  // });
+    // pause and play gifs
+      // $("img").click(
+      //   $(this).attr("src", response.data[i].images.fixed_width.url);
+      // );
+    
+  });
 
 });
