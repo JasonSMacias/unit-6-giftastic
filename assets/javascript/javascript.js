8819dddc416541c1bedcf502313fdff1567fcfe2
mzmzmz <44877972+JasonSMacias@users.noreply.github.com>
2019-01-07 19:55:53 -0500

    re-did buttons, according to instructions this time

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
-  // my giphyApi
-  var giphyApi = "BPXIt32b6aJqQBh3JzsEp3oaGeTjKYtW"
+  // my giphyApi including prefix and limit
+  var giphyApi = "&api_key=BPXIt32b6aJqQBh3JzsEp3oaGeTjKYtW&limit=10"
 
   // setting up giphy queryURL variable with beginning of url
   var queryURL = "https://api.giphy.com/v1/gifs/search?q=";
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function(response) {
-    // console.log(response);
-  });
+  // setting up array with strings for buttons
+  var topics = ["Darth Vader", "Princess Leia", "Luke Skywalker", "Obi-Wan Kenobi", "Han Solo", "R2-D2", "C-3PO", "Chewbacca", "Grand Moff Tarkin", "Emperor Palpatine", "Lando Calrissian", "Yoda", "Boba Fett", "Jabba the Hutt", "Admiral Ackbar", "Wicket (ewok)"];
+
+  for (i=0; i < topics.length; i++){
+    $("#gifbuttons").append(`<button class="btn btn-outline-dark btn-sm" type="button" name="" value="">${topics[i]}</button>`);
+  };
 
 
 
 
 
 
-
-
+  // $.ajax({
+  //   url: queryURL,
+  //   method: "GET"
+  // }).then(function(response) {
+  //   // console.log(response);
+  // });
 
 });
